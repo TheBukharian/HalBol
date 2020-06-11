63e4d3ddf56874fbca17753985c413f4f151c3dc
Asilbek Tilavov <thebukharianxo@mail.ru>
2020-06-11 16:01:22 +0500

    initial commit

---
+++ app/src/main/java/com/example/googlemapsexample/CamPic.kt
@@ -0,0 +1,134 @@
+package com.example.googlemapsexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.Manifest
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.ExifInterface
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_cam_pic.*
+
+
+
+
+
+
+
+
+class CamPic : AppCompatActivity() {
+
+            private val PERMISSION_CODE = 1000
+            private val IMAGE_CAPTURE_CODE = 1001
+            var image_uri: Uri? = null
+
+            override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                setContentView(R.layout.activity_cam_pic)
+
+
+                capture_btn.setOnClickListener {
+
+                    //if system os is Marshmallow or Above, we need to request runtime permission
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                        if (checkSelfPermission(Manifest.permission.CAMERA)
+                            == PackageManager.PERMISSION_DENIED ||
+                            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                            == PackageManager.PERMISSION_DENIED){
+
+                            //permission was not enabled
+                            val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                            //show popup to request permission
+                            requestPermissions(permission, PERMISSION_CODE)
+                        }
+                        else{
+                            //permission already granted
+                            openCamera()
+                        }
+                    }
+                    else{
+                        //system os is < marshmallow
+                        openCamera()
+                    }
+                }
+            }
+
+            private fun openCamera() {
+                val values = ContentValues()
+                values.put(MediaStore.Images.Media.TITLE, "New Picture")
+                values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera")
+                image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+                //camera intent
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+                startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
+            }
+
+
+            override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+
+                //called when user presses ALLOW or DENY from Permission Request Popup
+                when(requestCode){
+                    PERMISSION_CODE -> {
+                        if (grantResults.size > 0 && grantResults[0] ==
+                            PackageManager.PERMISSION_GRANTED){
+                            //permission from popup was granted
+                            openCamera()
+                        }
+                        else{
+
+                            //permission from popup was denied
+                            Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        //called when image was captured from camera intent
+
+        if (resultCode == Activity.RESULT_OK){
+            //set image captured to image view
+            PhotoCaptured.setImageURI(image_uri)
+
+
+            //get image description
+            val imageRealPath =  getRealPathFromURI(image_uri!!)
+
+
+            Toast.makeText(this,getRealPathFromURI(image_uri!!),Toast.LENGTH_LONG).show()
+
+            val exif = ExifInterface(imageRealPath)
+               Lat.text= exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE)
+               Long.text= exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE)
+
+
+
+        }
+    }
+
+    private fun getRealPathFromURI(contentURI: Uri): String? {
+        val result: String?
+        val cursor = contentResolver.query(contentURI, null, null, null, null)
+        if (cursor == null) { // Source is Dropbox or other similar local file path
+            result = contentURI.path
+        } else {
+            cursor.moveToFirst()
+            val idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA)
+            result = cursor.getString(idx)
+            cursor.close()
+        }
+        return result
+    }
+
+}
+
+
