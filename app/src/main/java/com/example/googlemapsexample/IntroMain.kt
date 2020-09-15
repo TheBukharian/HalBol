bd4da495dc6d14b74d6fc448ad6201379ad9133b
Asilbek Tilavov <thebukharianxo@mail.ru>
2020-09-15 19:21:11 +0500

    Splash screen done

--- app/src/main/java/com/example/googlemapsexample/IntroMain.kt
+++ app/src/main/java/com/example/googlemapsexample/IntroMain.kt
@@ -1,6 +1,7 @@
 package com.example.googlemapsexample
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
@@ -13,6 +14,7 @@ import androidx.viewpager2.widget.ViewPager2
 import com.example.googlemapsexample.Models.IntroScreen
 import com.example.googlemapsexample.Models.IntroScreenAdapter
 import kotlinx.android.synthetic.main.activity_intro_screen.*
+import java.lang.Boolean.getBoolean
 
 class IntroMain: AppCompatActivity() {
     private val introSliderAdapter=IntroScreenAdapter(
@@ -35,6 +37,21 @@ class IntroMain: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro_screen)
+
+                                                                                                        //check if it`s launching first time or not
+        val pref=getSharedPreferences("PREFERENCE", MODE_PRIVATE)
+        if(pref.getBoolean("isFirstRun",false)){
+            Intent(applicationContext,CamPic::class.java).also {
+                startActivity(it)
+                finish()
+            }
+        }else{
+            with(pref.edit()){
+                putBoolean("isFirstRun",true)
+                commit()
+            }
+        }
+
         introSliderViewPager2.adapter=introSliderAdapter
         setupIndicators()
         setcurrentIndicator(0)
